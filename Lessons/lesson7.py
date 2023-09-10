7242278ba3e58a8ffd9cf760a0049275881c2aa8
stovik <jenda.stovik@gmail.com>
2023-09-10 16:01:31 +0900

    quizz 7

--- Lessons/lesson7.py
+++ Lessons/lesson7.py
@@ -43,7 +43,18 @@ price = 0
 for it in order.keys():
     price += prices[it]
 
-print(str(price).center(50, '-'))
+#print(str(price).center(50, '-'))
+
+import random
+def rand():
+    con = True
+    times = 0
+    while con:
+        n = random.randint(1, 100)
+        times += 1
+        if n <= 15:
+            con = False
+    return times
 
 
 
--- Quizzes/Quiz_007.md
+++ Quizzes/Quiz_007.md
@@ -2,6 +2,16 @@
 
 ## Python code
 ```python
+import random
+def rand():
+    con = True
+    times = 0
+    while con:
+        n = random.randint(1, 100)
+        times += 1
+        if n <= 15:
+            con = False
+    return times
 ```
 
 ## Output
