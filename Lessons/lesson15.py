e7e1175ece4162012d12f35d24f74689a05a4b0f
stovik <jenda.stovik@gmail.com>
2023-09-28 17:47:12 +0900

    q15

--- Lessons/lesson15.py
+++ Lessons/lesson15.py
@@ -4,3 +4,16 @@ def door_flipper(N):
         for d in range(stu, N, stu+1):
             doors[d] = not doors[d]
     return doors
+
+def ran(L, N):
+    for _ in range(L):
+        N = N // L
+    if N == 0 or L%2 == 1:
+        return (L, L%2 == 1)
+    else:
+        return (N, True)
+
+def run_ran(N, L):
+    print(f"{L} {N} --> {ran(L,N)}")
+
+run_ran(999, 5)
